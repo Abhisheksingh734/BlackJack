73dc1591135fb8d012cc3f20a94c81aa22705a71
Abhishek Singh <114075050+Abhisheksingh734@users.noreply.github.com>
2024-11-21 23:02:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,104 +1,349 @@
-const generateDeck=()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  const playerTotal = document.querySelector("#playerTotal");
+  const dealerTotal = document.querySelector("#dealerTotal");
 
-    const cards = ["2","3","4","5","6","7","8","9","10","King","Queen","Jack","Ace"]
-    const suits = ["Heart","Spade","Club","Diamond"]
-    
-    let deck = [];
-    
-    for(let card of cards){
-        for(let suit of suits){
-            deck.push({Card:card,Suit:suit})
-        }
+  const cardsValues = {
+    0: "A",
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: "K",
+    11: "D",
+    12: "J",
+  };
+  const cardKeys = Object.keys(cardsValues);
+
+  const topSection = document.querySelector(".topSection");
+  const botSection = document.querySelector(".botSection");
+
+  const moreMoneyBtn = document.querySelector("#moreMoney");
+  const moreMoneyBtn2 = document.querySelector("#moreMoney2");
+  const playerMoney = document.querySelector("#playerMoney");
+
+  const valueToPlay = document.querySelector("#valueToPlay");
+  const playBtn = document.querySelector("#playBtn");
+
+  const amountsMoney = document.querySelectorAll(".amounts button");
+
+  const btnSpace = document.querySelector(".btnSpace");
+  const continuePlay = document.querySelector("#continuePlaying");
+  const stopPlay = document.querySelector("#stopPlaying");
+
+  //AMOUNTS
+  let moneyToAdd = 1;
+  valueToPlay.innerHTML = `Value: ${moneyToAdd}₹`;
+
+  amountsMoney.forEach((amount) => {
+    amount.addEventListener("click", () => {
+      moneyToAdd = parseInt(amount.dataset.value);
+      if (moneyToAdd === 0o0000) {
+        moneyToAdd = money;
+        valueToPlay.innerHTML = `Value: ${money}₹`;
+      } else {
+        valueToPlay.innerHTML = `Value: ${moneyToAdd}₹`;
+      }
+    });
+  });
+
+  moreMoneyBtn2.addEventListener("click", () => {
+    moneyToAdd += 1;
+    valueToPlay.innerHTML = `Value: ${moneyToAdd}₹`;
+  });
+
+  //PLAY BTN
+  playBtn.addEventListener("click", () => {
+    if (moneyToAdd > money) {
+      alert("No more money buddyyy");
+      return;
+    } else {
+      money -= moneyToAdd;
+      playerMoney.innerHTML = `Money: ${money}₹`;
     }
-    return deck;
-};
+    playBtn.disabled = true;
+    btnSpace.style.display = "flex";
 
-
-const drawCard = (deck)=>{
-	
-	let randomInd = Math.floor(Math.random() * deck.length)
-	
-    let cards = deck[randomInd];
-	
-    deck.splice(randomInd, 1);
-	
-    //randomInd se start hoga, 1 item delete krega 
-    return cards;
-	
-}
-
-const checkScore=(hand)=>{
-	let total = 0;
-    for(const cardObj of hand){
-		if(cardObj.Card === "King"){
-			total += 10;
-        }
-        else if(cardObj.Card === "Queen"){
-			total += 10;
-        }
-        else if(cardObj.Card === "Jack"){
-			total += 10;
-        }
-        else if(cardObj.Card === "Ace"){
-			total += 1;
-        }
-        else{
-			total += Number(cardObj.Card);
-        }
+    for (let i = 0; i <= 1; i++) {
+      var newDiv2 = document.createElement("div");
+      newDiv2.className = "playerHand";
+      botSection.appendChild(newDiv2);
+      newDiv2.innerHTML = randomCardValuePlayer();
+      setTimeout(() => checkTotal(), 100);
     }
-    return total;
-	
-}
-// const myCards = drawCard(deck);
-const deck = generateDeck();
+    for (let i = 0; i <= 0; i++) {
+      var newDiv = document.createElement("div");
+      newDiv.className = "dealerHand";
+      topSection.appendChild(newDiv);
+      newDiv.innerHTML = randomCardValueDealer();
+      setTimeout(() => checkTotal(), 100);
+    }
+  });
 
+  //CONTINUE PLAYING
 
-const playerHand = [];
-const dealerHand = [];
+  continuePlay.addEventListener("click", () => {
+    for (let i = 0; i <= 0; i++) {
+      var newDiv2 = document.createElement("div");
+      newDiv2.className = "playerHand";
+      botSection.appendChild(newDiv2);
+      newDiv2.innerHTML = randomCardValuePlayer();
+      if (totalplayer <= 21 && totalplayer !== 21) {
+        if (hasA >= 1 && totalplayer > 21) {
+          hasA -= 1;
+          totalplayer -= 10;
+        }
+      }
+      setTimeout(() => checkTotal(), 100);
+    }
+  });
 
-playerHand.push(drawCard(deck));
-playerHand.push(drawCard(deck));
-dealerHand.push(drawCard(deck));
-dealerHand.push(drawCard(deck));
+  //PLAYER MONEY
 
+  let money = 20;
+  playerMoney.innerHTML = `Money: ${money}₹`;
 
+  moreMoneyBtn.addEventListener("click", () => {
+    money += 1;
+    playerMoney.innerHTML = `Money: ${money}₹`;
+  });
 
-console.log("Starting Player Hand: ",playerHand);
-console.log("Starting Player Score: ",checkScore(playerHand));
-console.log("Starting dealer Hand: ",dealerHand);
-console.log("Starting dealer Score: ",checkScore(dealerHand));
-while (true){
-	playerHand.push(drawCard(deck));
-	//deal player card
-	const playerScore = checkScore(playerHand);
-	let dealerScore = checkScore(dealerHand);
+  stopPlay.addEventListener("click", () => {
+    dealerTimeout = setTimeout(() => dealerGOO(), 500);
+    continuePlay.disabled = true;
+    stopPlay.disabled = true;
+  });
 
-	if(playerScore>21){
-		console.log(`You lose! Your final score was ${playerScore} while dealer had ${dealerScore}`);
-		break;
-	}
+  //CHECK TOTAL TO DEALER PLAY
+  function checkTotal() {
+    if (totalplayer > 21 || totalplayer === 21) {
+      dealerTimeout = setTimeout(() => dealerGOO(), 500);
+      continuePlay.disabled = true;
+      stopPlay.disabled = true;
+    }
+  }
 
-	if(playerScore === 21){
-		console.log(`You Win! Your final score was ${playerScore} while dealer had ${dealerScore}`);
-		break;
-	}
+  function dealerGOO() {
+    var newDiv = document.createElement("div");
+    newDiv.className = "dealerHand";
+    topSection.appendChild(newDiv);
+    newDiv.innerHTML = randomCardValueDealer();
 
-	dealerHand.push(drawCard(deck));
+    if (totaldealer === 21) {
+      clearTimeout(dealerTimeout);
+      setTimeout(() => checkWin(), 1200);
+    }
 
-	dealerScore = checkScore(dealerHand);
+    if (totaldealer < 21) {
+      if (hasA >= 1 && totaldealer > 21) {
+        hasA -= 1;
+        totaldealer -= 10;
+      }
 
-	if(dealerScor>21){
-		console.log(`You Win! Your final score was ${playerScore} while dealer had ${dealerScore}`);
-		break;
-	}
-	if(dealerScore === 21){
-		console.log(`You lose! Your final score was ${playerScore} while dealer had ${dealerScore}`);
-		break;
-	}
-	
-}
+      // Clear the previous timeout
+      clearTimeout(dealerTimeout);
 
-console.log("Ending Player Hand: ",playerHand);
-console.log("Ending Player Score: ",checkScore(playerHand));
-console.log("Ending dealer Hand: ",dealerHand);
-console.log("Ending dealer Score: ",checkScore(dealerHand));
+      // Start a new timeout
+      dealerTimeout = setTimeout(() => dealerGOO(), 1000);
+    } else {
+      setTimeout(() => checkWin(), 1200);
+    }
+
+    if (totaldealer >= 16 && totaldealer <= 21 && totaldealer > totalplayer) {
+      // Stop the recursion when the condition is met
+      clearTimeout(dealerTimeout);
+      setTimeout(() => checkWin(), 1200);
+    }
+  }
+
+  // Declare dealerTimeout variable outside the function
+  let dealerTimeout;
+
+  //VERIFY WINNER
+
+  function checkWin() {
+    if (totalplayer > 21) {
+      alert("YOU LOSE");
+      playerMoney.innerHTML = `Money: ${money}₹`;
+      resetGame();
+      return;
+    } else if (
+      (totaldealer > 21 && totalplayer <= 21) ||
+      (totalplayer <= 21 && totalplayer > totaldealer)
+    ) {
+      alert("YOU WON");
+      money += moneyToAdd * 2;
+      playerMoney.innerHTML = `Money: ${money}₹`;
+      resetGame();
+      return;
+    } else if (totalplayer === 21 && totaldealer === 21) {
+      alert("DRAW");
+      money += moneyToAdd;
+      playerMoney.innerHTML = `Money: ${money}₹`;
+      resetGame();
+      return;
+    } else if (
+      totaldealer >= 16 &&
+      totaldealer <= 21 &&
+      totaldealer > totalplayer
+    ) {
+      alert("YOU LOSE");
+      playerMoney.innerHTML = `Money: ${money}₹`;
+      resetGame();
+      return;
+    }
+  }
+
+  //RESET GAME
+  function resetGame() {
+    const botChildren = Array.from(botSection.children);
+    const topChildren = Array.from(topSection.children);
+
+    botChildren.forEach((child) => {
+      botSection.removeChild(child);
+    });
+
+    topChildren.forEach((child) => {
+      topSection.removeChild(child);
+    });
+
+    playBtn.disabled = false;
+    btnSpace.style.display = "none";
+    continuePlay.disabled = false;
+    stopPlay.disabled = false;
+
+    totalplayer = 0;
+    totaldealer = 0;
+    hasA = 0;
+
+    playerTotal.innerHTML = `Total: ${totaldealer}`;
+    dealerTotal.innerHTML = `Total: ${totaldealer}`;
+  }
+
+  //SET VALUES AND SET TEXT CARD
+  let totalplayer = 0;
+  let totaldealer = 0;
+
+  function randomCardValuePlayer() {
+    let randomIndex = Math.floor(Math.random() * cardKeys.length);
+    let randomKey = randomIndex;
+    let randomCardText = cardsValues[randomKey];
+
+    switch (randomKey) {
+      case 0:
+        let aux = totalplayer + 11;
+        if (aux > 21) {
+          hasA += 1;
+          totalplayer += 1;
+          break;
+        } else {
+          hasA += 1;
+          totalplayer += 11;
+          break;
+        }
+      case 1:
+        totalplayer += 1;
+        break;
+      case 2:
+        totalplayer += 2;
+        break;
+      case 3:
+        totalplayer += 3;
+        break;
+      case 4:
+        totalplayer += 4;
+        break;
+      case 5:
+        totalplayer += 5;
+        break;
+      case 6:
+        totalplayer += 6;
+        break;
+      case 7:
+        totalplayer += 7;
+        break;
+      case 8:
+        totalplayer += 8;
+        break;
+      case 9:
+        totalplayer += 9;
+        break;
+      case 10:
+        totalplayer += 10;
+        break;
+      case 11:
+        totalplayer += 10;
+        break;
+      case 12:
+        totalplayer += 10;
+        break;
+    }
+
+    playerTotal.innerHTML = `Total: ${totalplayer}`;
+    return randomCardText;
+  }
+
+  let hasA = 0;
+
+  function randomCardValueDealer() {
+    let randomIndex = Math.floor(Math.random() * cardKeys.length);
+    let randomKey = randomIndex;
+    let randomCardText = cardsValues[randomKey];
+    switch (randomKey) {
+      case 0:
+        let aux = totaldealer + 11;
+        if (aux > 21) {
+          totaldealer += 1;
+          break;
+        } else {
+          hasA += 1;
+          totaldealer += 11;
+          break;
+        }
+      case 1:
+        totaldealer += 1;
+        break;
+      case 2:
+        totaldealer += 2;
+        break;
+      case 3:
+        totaldealer += 3;
+        break;
+      case 4:
+        totaldealer += 4;
+        break;
+      case 5:
+        totaldealer += 5;
+        break;
+      case 6:
+        totaldealer += 6;
+        break;
+      case 7:
+        totaldealer += 7;
+        break;
+      case 8:
+        totaldealer += 8;
+        break;
+      case 9:
+        totaldealer += 9;
+        break;
+      case 10:
+        totaldealer += 10;
+        break;
+      case 11:
+        totaldealer += 10;
+        break;
+      case 12:
+        totaldealer += 10;
+        break;
+    }
+
+    dealerTotal.innerHTML = `Total: ${totaldealer}`;
+    return randomCardText;
+  }
+  //end
+});
